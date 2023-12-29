52f7cf968178ad252987cab26c5e501edd8d1002
RyanJBrenner <143837787+RyanJBrenner@users.noreply.github.com>
2023-12-29 15:33:08 -0600

    minor updates
    changes width of cards on main pages

--- components/misc/NewYorkLayout.tsx
+++ components/misc/NewYorkLayout.tsx
@@ -8,7 +8,7 @@ export function FileCard({ folder, file }) {
     <section className="w-full py-3">
       <div className="container px-2 md:px-2">
         <div className="grid">
-          <div className="lg:min-h-80 md:min-h-80 md:mx-auto mx-8 w-full sm:min-h-64 p-6 md:p-8 rounded-xl bg-slate-200 overflow-hidden shadow-lg flex flex-col justify-between">
+          <div className="lg:min-h-80 md:min-h-80 md:mx-auto mx-8 w-auto md:w-full sm:min-h-64 p-6 md:p-8 rounded-xl bg-slate-200 overflow-hidden shadow-lg flex flex-col justify-between">
             <h3 className="text-2xl font-bold bg-clip-text4 my-4">{file.title}</h3>
             <p className="text-ellipsis line-clamp-4 my-4">{file.excerpt}</p>
             <div className="my-4">
